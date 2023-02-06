fd9c936c4b5bb0a7e14e88112de8143cb619654a
loque18 <madridx2010@gmail.com>
2023-02-06 18:13:30 -0500

    refactor: refactored style (bulma custom vars)

--- src/app/layout/services/layout.service.ts
+++ src/app/layout/services/layout.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { LayoutModule } from 'src/app/layout/layout.module';
-
 @Injectable({
     providedIn: 'root',
 })
