d8a1e7fff2c878ea66f90d082d68bf3b2d9c4eb4
Ghadi <yobiz668@gmail.com>
2023-03-02 17:37:23 +0200

    multi tabs and font size fixes

--- src/app/components/internal/bets-tabs/bets-tabs.component.ts
+++ src/app/components/internal/bets-tabs/bets-tabs.component.ts
@@ -10,12 +10,9 @@ export class BetsTabsComponent {
 
 links: Ibets[] = latestBets;
 activeTab : string = 'All';
-color = document.getElementById("color");
 
  activateTab(tabs : string) : void{
     this.activeTab = tabs;
-    this.color?.classList.add('is-active')
-    console.log(this.color)
-    console.log('what')
     } 
+
 }
